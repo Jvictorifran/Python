5926415296a04f838e0f5cf870ecba756f72c90b
Lezxit <joaovictorcandidoifran@gmail.com>
2025-03-21 16:55:28 -0400

    dia 4

---
+++ aula20.py
@@ -0,0 +1,13 @@
+#interpolação 
+
+nome = 'lEZXIT'
+preço = 17.545456
+ 
+variavel = '%s, o preço é R$%.2f' % (nome, preço)
+
+print(variavel)
+
+#é possivel usar o hexadecilal
+# x e X
+
+print ('o hexadecimal de %d é %04X' % (13,13))
